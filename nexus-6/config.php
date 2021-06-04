f53a556fa1aee2b4d98b7e8d7fe75bd9e6cfeda3
unknown <eduardoroyot@gmail.com>
2021-06-04 08:26:17 +0200

    Changed timezone.

--- nexus-6/config.php
+++ nexus-6/config.php
@@ -5,7 +5,7 @@ error_reporting(E_ALL);
 ini_set('display_errors', 1);
 
 define('SITE_URL', 'http://localhost:8888/cms/nexus-6');
-define('SITE_TIMEZONE', 'Australia/Sydney');
+define('SITE_TIMEZONE', 'Europe/Madrid');
 define('SITE_LANG', ['es', 'spa', 'es_ES']);
 
 // Conexión a Base de Datos
@@ -16,4 +16,4 @@ define('DB_DATABASE', 'microcms');
 define('DB_PORT', '8889');
 
 define('ADMIN_USER', 'eduardo');
-define('ADMIN_PASS', '123');
+define('ADMIN_PASS', 'god');
